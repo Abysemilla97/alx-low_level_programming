2eba488bd1627edf40f769c64130c25482a22c9a
Abigail Simon <abby.cymon@gmsil.com>
2023-04-22 20:28:25 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - print single digit in base 10 with putchar
+ *Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+
+	putchar('\n');
+	return (0);
+}
