5e92d54964d90791107f863bc35ddf8000df99fd
Abigail Simon <abby.cymon@gmsil.com>
2023-04-26 06:51:52 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
 #include "main.h"
-/**
- *main - prints 10 times the alphabet in lowercase
- *Return: always 0
- */
- void print_alphabet_x10(void)
-{
-	int e;
-	int r;
 
-	for (r = 1; e <= 10; e++)
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
 	{
-		for (r = 97; r <= 122; r++)
+		for  (j = 'a'; j <= 'z'; j++)
 		{
-			_putchar(e);
+			_putchar(j);
 		}
-		_putchar ('\n');
 	}
+	_putchar('\n');
 }
