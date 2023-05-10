84e1e2aedcfa4278d4a5e170b5ee951194ee251d
Abigail Simon <abby.cymon@gmsil.com>
2023-05-10 14:03:53 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function like puts().
+ * @s: input
+ * Return: always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
+
