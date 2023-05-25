7dfd82b078e1bcd1b2411095580830b5264479f0
Abigail Simon <abby.cymon@gmsil.com>
2023-05-25 12:51:16 -0700

    task 2- Write a function that prints strings, \n.

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,15 +19,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(list, char *);
-		if (!str)
-			str = "nil";
 
-		if (!separator)
+		if (str)
 			printf("%s", str);
-		else if (separator && i == 0)
-			printf("%s", str);
-			else
-				printf("%s%s", separator, str);
+		else
+			printf("(nil)");
+
+		if (i < n - 1)
+			if (separator)
+				printf("%s", separator);
 	}
 	printf("\n");
 	va_end(list);
