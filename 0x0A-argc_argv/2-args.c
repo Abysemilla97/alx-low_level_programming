952e98514918eab914f99032e7db2563e0137060
Abigail Simon <abby.cymon@gmsil.com>
2023-06-02 14:31:42 -0700

    removed directory

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints all the argument received.
- * @argc: this counts arguments.
+ * main - prints all the argument received
+ * @argc: this counts arguments
  * @argv: this is vector, array list.
  * Return: always 0.
  */
 int main(int argc, char *argv[])
 {
-	int a;
+	int i = 0;
 
-	for (a = 0; a < argc; a++)
+	for (; i < argc; i++)
 	{
-		printf("%s\n", argv[a]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
