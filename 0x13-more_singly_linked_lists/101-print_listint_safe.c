e5079431f785663c75d7d99fbe700c945b05e45d
Abigail Simon <abby.cymon@gmsil.com>
2023-06-06 15:48:37 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * r - reallocates memory for the array.
+ * _r - reallocates memory for the array.
  * @list: the list to append.
  * @size: the size of the new list.
  * @new: the new node to add to the list.
@@ -11,20 +11,20 @@
 const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 {
 	size_t m;
-	const listint_t **n_list;
+	const listint_t **newlist;
 
-	n_list = malloc(size * sizeof(listint_t *));
-	if (n_list == NULL)
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
 	{
 		free(list);
 		exit(98);
 	}
 	for (m = 0; m < size - 1; m++)
-		n_list[m] = list[m];
+		newlist[m] = list[m];
 
-	n_list[m] = new;
+	newlist[m] = new;
 	free(list);
-	return (n_list);
+	return (newlist);
 }
 
 /**
@@ -51,6 +51,7 @@ size_t print_listint_safe(const listint_t *head)
 	}
 	numb++;
 	list = _r(list, numb, head);
+	printf("[%p] %d\n", (void *)head, head->n);
 	head = head->next;
 	}
 	free(list);
