1b8f870b97aa761247c7fbba37c308e3d86cc16e
Abysemilla97 <abby.cymon@gmail.com>
2023-07-07 18:17:11 -0700

    resolved all error on libmy.a file

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -3,7 +3,28 @@
 #include "stdlib.h"
 #include "string.h"
 
+/**
+ * _atoi - converting a string into an integer.
+ * @s: the string to use.
+ * Return: the integer.
+ */
 int _atoi(char *s)
 {
-	return atoi(s);
+	int sg = 1;
+	int i = 0;
+	unsigned int rs = 0;
+
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+		sg *= -1;
+		i++;
+	}
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		rs = (rs * 10) + (s[i] - '0');
+		i++;
+	}
+	rs *= sg;
+	return (rs);
 }
--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -3,7 +3,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * _strcpy - a function that copies the string pointer.
+ * @dest: the destination.
+ * @src: the source.
+ * Return: the string.
+ */
 char *_strcpy(char *dest, char *src)
 {
-	return strcpy(dest, src);
+	int l = 0;
+	int x = 0;
+
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for (; x < l; x++)
+	{
+		dest[x] =  src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
 }
