c2de89b46ba8bb988c8c02f4ff6d1c5df2aee92b
Abysemilla97 <abby.cymon@gmail.com>
2023-07-13 17:18:22 -0700

    a function that concatenates two strings for more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,13 +10,17 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = strlen(s1), len2 = strlen(s2);
+	unsigned int len1, len2;
 	char *strg;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
 
 	if (n >= len2)
 		n = len2;
