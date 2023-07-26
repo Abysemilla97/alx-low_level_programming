a1d3cc7632ba37f7d637286673849417e4cc8084
Abysemilla97 <abby.cymon@gmail.com>
2023-07-26 10:58:45 -0700

    a function that works with powered num

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * pow_recursion - 
+ * @x: the value.
+ * @y: the power.
+ * Return: the power 
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	/*get what will add always until x  or y = 0, and mult by 1*/
+	return (x * _pow_recursion(x, y - 1));
+}
