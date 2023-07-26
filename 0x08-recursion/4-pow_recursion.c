bc21328e5e6acac5129b1f4a6a624ce38a39c703
Abysemilla97 <abby.cymon@gmail.com>
2023-07-26 11:06:08 -0700

    a function that works with powered num

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * pow_recursion - 
+ * _pow_recursion - function returning the value of x raised to the power of y.
  * @x: the value.
  * @y: the power.
- * Return: the power 
+ * Return: the power.
  */
 int _pow_recursion(int x, int y)
 {
