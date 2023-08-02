975f2c23691d62561cfb533b9cd13ed3cd9c3cec
Abysemilla97 <abby.cymon@gmail.com>
2023-08-02 08:07:02 -0700

    freeing the element of list.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - a function freeing a listint list.
+ * @head: the derefenced pointer to the head.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *now;
+
+	while (*head != NULL)
+	{
+		now = *head;
+		*head = (*head)->next;
+		free(now);
+	}
+}
